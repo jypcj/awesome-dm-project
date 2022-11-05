b9d4712aa951d42a877f96d771d58ec5312db925
jiangyunpeng <1907834168@qq.com>
2022-11-05 21:45:08 +0800

    init: init the project

---
+++ data.py
@@ -0,0 +1,74 @@
+import json
+import scipy.io as sio
+import scipy.sparse as sp
+import numpy as np
+import torch
+from sklearn import preprocessing
+
+from utils import normalize, sparse_matrix2torch_sparse_tensor
+
+
+def data_preprocess(dataset: str):
+    """pre-process the data before training
+
+        Parameter:
+            dataset: the name of dataset, including 'Amazon_eletronics', 'dblp', 'cora-full' and 'ogbn-arxiv'
+        Return:
+
+    """
+    train_class_list: list = list()
+    test_class_list: list = list()
+    valid_class_list: list = list()
+    train_class_list, valid_class_list, test_class_list = json.load(open('./dataset/{}_class_split.json'.format(dataset)))
+    if dataset == "Amazon_eletronics" or dataset == 'dblp':
+        # all the edges in graph is denoted by (node1[i], node2[i])
+        node1: list = list()
+        node2: list = list()
+        for line in open("./dataset/{}_network".format(dataset)):
+            n1, n2 = line.strip().split("\t")
+            node1.append(int(n1))
+            node2.append(int(n2))
+        node_number: int = max(max(node1), max(node2)) + 1
+
+        # data_train and data_test are dicts, they have useful keys 'Index', 'Label' and 'Attributes'
+        # Index: [[1,2,3...]]
+        # Label: [[1],[1],[2],[2],...]
+        # Attributes: matrix
+        data_train: dict = sio.loadmat("./dataset/{}_train.mat".format(dataset))
+        data_test: dict = sio.loadmat("./dataset/{}_test.mat".format(dataset))
+        # label of nodes
+        labels = np.zeros((node_number, 1))
+        labels[data_train['Index']] = data_train["Label"]
+        labels[data_test['Index']] = data_test["Label"]
+        # feature matrix
+        features_matrix = np.zeros((node_number, data_train["Attributes"].shape[1]))
+        features_matrix[data_train['Index']] = data_train["Attributes"].toarray()
+        features_matrix[data_test['Index']] = data_test["Attributes"].toarray()
+        # adjacency matrix
+        adjacency_matrix = sp.coo_matrix((np.ones(len(node1)), (node1, node2)), shape=(node_number, node_number))
+
+        # all the classes in a list
+        class_list: list = []
+        for cls in labels:
+            if cls[0] not in class_list:
+                class_list.append(cls[0])
+        # class_id -> [node_id, node_id, ...]
+        class_dict: dict = {}
+        for cls in class_dict:
+            class_dict[cls] = []
+        for node_id, class_id in labels:
+            class_dict[class_id[0]].append(node_id)
+
+        label_binarizer = preprocessing.LabelBinarizer()
+        labels = label_binarizer.fit_transform(labels)
+        features_matrix = torch.FloatTensor(features_matrix)
+        labels = torch.LongTensor(np.where(labels)[1])
+        adjacency_matrix = sparse_matrix2torch_sparse_tensor(normalize(adjacency_matrix + sp.eye(adjacency_matrix.shape[0])))
+    elif dataset == 'cora-full':
+        pass
+    elif dataset == 'ogbn-arxiv':
+        pass
+
+
+if __name__ == '__main__':
+    data_preprocess("Amazon_eletronics")
---
+++ model.py
@@ -0,0 +1,151 @@
+import torch.nn
+import math
+from torch.nn.parameter import Parameter
+import numpy as np
+
+
+# node-level adaption model
+class GraphConvolutionNode(torch.nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolutionNode, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+
+        stdv = 1. / math.sqrt(self.weight.size(1))
+
+        self.weight.data.uniform_(-stdv, stdv)
+
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+
+# used in node-level adaption
+class GCN4NodeLevel(torch.nn.Module):
+    # 构建模型
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN4NodeLevel, self).__init__()
+
+        self.gc1 = GraphConvolutionNode(nfeat, nhid)
+        self.gc2 = GraphConvolutionNode(nhid, nhid)
+        self.dropout = dropout
+
+    # 前向传播
+    def forward(self, x, adj):
+        return self.gc1(x, adj)
+
+
+# node-level adaption model
+class GraphConvolutionClass(torch.nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolutionClass, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+
+        self.weight.data.uniform_(-stdv, stdv)
+
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj, w, b):
+        if w is None and b is None:
+            alpha_w = alpha_b = beta_w = beta_b = 0
+        else:
+            alpha_w = w[0]
+            beta_w = w[1]
+            alpha_b = b[0]
+            beta_b = b[1]
+
+        support = torch.mm(input, self.weight * (1 + alpha_w) + beta_w)  # formula (6)
+        output = torch.mm(adj, support)
+
+        if self.bias is not None:
+            return output + self.bias * (1 + alpha_b) + beta_b
+        else:
+            return output
+
+
+# used in node-level adaption
+class GCN4ClassLevel(torch.nn.Module):
+    def __init__(self, nfeat, nhid, dropout):
+        super(GCN4ClassLevel, self).__init__()
+
+        self.gc1 = GraphConvolutionClass(nfeat, nhid)
+        self.gc2 = GraphConvolutionClass(nhid, nhid)
+        self.generater = torch.nn.Linear(nfeat, (nfeat + 1) * nhid * 2 + (nhid + 1) * nhid * 2)
+
+        self.dropout = dropout
+
+    def permute(self, input_adj, input_feat, drop_rate=0.1):
+        # return input_adj
+
+        adj_random = torch.rand(input_adj.shape).cuda() + torch.eye(input_adj.shape[0]).cuda()
+
+        feat_random = np.random.choice(input_feat.shape[0], int(input_feat.shape[0] * drop_rate),
+                                       replace=False).tolist()
+
+        masks = torch.zeros(input_feat.shape).cuda()
+        masks[feat_random] = 1
+
+        random_tensor = torch.rand(input_feat.shape).cuda()
+
+        return input_adj * (adj_random > drop_rate), input_feat * (1 - masks) + random_tensor * masks
+
+    def forward(self, x, adj, w1=None, b1=None, w2=None, b2=None):
+        x = torch.nn.functional.relu(self.gc1(x, adj, w1, b1))
+        x = torch.nn.functional.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj, w2, b2)
+        return x
+
+
+# used in classifier
+class Linear(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super(Linear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        self.bias = Parameter(torch.FloatTensor(out_features))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+
+        self.weight.data.uniform_(-stdv, stdv)
+
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, w=None, b=None):
+        if w != None:
+            return torch.mm(input, w) + b
+        else:
+            return torch.mm(input, self.weight) + self.bias
